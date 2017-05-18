31c1146066e6064da5421cba999bb052447c5d1e
WCSIsabelle <ismuligo@gmail.com>
2017-05-18 18:19:56 +0200

    creation bundles MyOrleansBundle et BlogBundle

---
+++ src/MyOrleansBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MyOrleansBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('MyOrleansBundle:Default:index.html.twig');
+    }
+}
---
+++ src/MyOrleansBundle/MyOrleansBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MyOrleansBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MyOrleansBundle extends Bundle
+{
+}
