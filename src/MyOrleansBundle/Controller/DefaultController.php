61ce41e2db44bdc40bb7801915b926297ca18e70
HageraN <hagii.neche@gmail.com>
2017-05-31 14:18:34 +0200

    Ajout du stickynav.js

--- src/MyOrleansBundle/Controller/DefaultController.php
+++ src/MyOrleansBundle/Controller/DefaultController.php
@@ -12,7 +12,7 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('MyOrleansBundle::index-bis.html.twig');
+        return $this->render('MyOrleansBundle::index-bis-bis.html.twig');
     }
 
     /**
