0a2a8eaec9fc0df45a479806a2cdd868198b156c
jbh225 <jb.hallassou@gmail.com>
2017-07-04 15:53:07 +0200

    ajout formulaire sur page residences et appartement

--- src/MyOrleansBundle/Controller/front/AgenceController.php
+++ src/MyOrleansBundle/Controller/front/AgenceController.php
@@ -8,12 +8,14 @@
 
 namespace MyOrleansBundle\Controller\front;
 
+use MyOrleansBundle\Entity\Client;
 use MyOrleansBundle\Entity\Collaborateur;
 use MyOrleansBundle\Entity\Evenement;
 use MyOrleansBundle\Entity\Media;
 use MyOrleansBundle\Entity\Partenaire;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class AgenceController extends Controller
@@ -21,19 +23,53 @@ class AgenceController extends Controller
     /**
      * @Route("/agence", name="agence")
      */
-    public function agencyAction(SessionInterface $session)
+    public function agencyAction(SessionInterface $session, Request $request)
     {
+        $client = new Client();
+
         $parcours = null;
         if ($session->has('parcours')) {
             $parcours = $session->get('parcours');
         }
 
+
+        $telephoneNumber = $this->getParameter('telephone_number');
         $em = $this->getDoctrine()->getManager();
 
         $partenaires = $em->getRepository(Partenaire::class)->findAll();
         $collaborateurs = $em->getRepository(Collaborateur::class)->findAll();
         $evenements =$em->getRepository(Evenement::class)->findAll();
         $cover = $em->getRepository(Media::class)->findAll();
+        $formulaire = $this->createForm('MyOrleansBundle\Form\FormulaireType', $client);
+
+        $formulaire->handleRequest($request);
+
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $mailer = $this->get('mailer');
+
+            $message = new \Swift_Message('Nouveau message de my-orleans.com');
+            $message
+                ->setTo($this->getParameter('mailer_user'))
+                ->setFrom($this->getParameter('mailer_user'))
+
+                ->setBody(
+                    $this->renderView(
+
+                        'MyOrleansBundle::receptionForm.html.twig',
+                        array('client' => $client)
+                    ),
+                    'text/html'
+                );
+
+            $mailer->send($message);
+
+            $em->persist($client);
+            $em->flush();
+
+            return $this->redirectToRoute('immo_pratique');
+        }
 
         return $this->render('MyOrleansBundle::agence.html.twig',
             [
@@ -41,7 +77,9 @@ class AgenceController extends Controller
                 'partenaires' => $partenaires,
                 'collaborateurs'=>$collaborateurs,
                 'evenements'=>$evenements,
-                'cover'=>$cover
+                'telephone_number' => $telephoneNumber,
+                'cover'=>$cover,
+                'form' => $formulaire->createView()
             ]
         );
     }
