efe22916dc4ea8e33db901668f035990034db15d
Nico P <nicopinsard@gmail.com>
2017-07-05 10:32:21 +0200

    modify home controller with flat and residence controller add

---
+++ src/MyOrleansBundle/Controller/front/ResidencesController.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder8
+ * Date: 27/06/17
+ * Time: 11:52
+ */
+
+namespace MyOrleansBundle\Controller\front;
+
+
+use MyOrleansBundle\Entity\Article;
+
+use MyOrleansBundle\Entity\CategoriePresta;
+use MyOrleansBundle\Entity\Flat;
+use MyOrleansBundle\Entity\Media;
+
+use MyOrleansBundle\Entity\Client;
+use MyOrleansBundle\Entity\Collaborateur;
+use MyOrleansBundle\Entity\Evenement;
+
+use MyOrleansBundle\Entity\Pack;
+use MyOrleansBundle\Entity\Prestation;
+use MyOrleansBundle\Entity\Service;
+use MyOrleansBundle\Entity\Temoignage;
+use MyOrleansBundle\Entity\Residence;
+use MyOrleansBundle\Entity\TypePresta;
+use MyOrleansBundle\Entity\Ville;
+use MyOrleansBundle\Form\SimpleSearchType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+
+class ResidencesController extends Controller
+{
+    /**
+     * @Route("/residences/{id")
+     */
+    public function residence($id, SessionInterface $session, Request $request)
+    {
+        $parcours = null;
+        if ($session->has('parcours')) {
+            $parcours = $session->get('parcours');
+        }
+
+        // Formulaire de contact
+        $client = new  Client();
+        $formulaire = $this->createForm('MyOrleansBundle\Form\FormulaireType', $client);
+        $telephoneNumber = $this->getParameter('telephone_number');
+        $formulaire->handleRequest($request);
+
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $mailer = $this->get('mailer');
+
+            $message = new \Swift_Message('Nouveau message de my-orleans.com');
+            $message
+                ->setTo($this->getParameter('mailer_user'))
+                ->setFrom($this->getParameter('mailer_user'))
+                ->setBody(
+                    $this->renderView(
+
+                        'MyOrleansBundle::receptionForm.html.twig',
+                        array('client' => $client)
+                    ),
+                    'text/html'
+                );
+
+            $mailer->send($message);
+
+            $em->persist($client);
+            $em->flush();
+            return $this->redirectToRoute('residences');
+        }
+
+        return $this->render('MyOrleansBundle::residence.html.twig', [
+            'parcours' => $parcours,
+            'telephone_number' => $telephoneNumber,
+            'form' => $formulaire->createView()
+        ]);
+
+        $em = $this->getDoctrine()->getManager();
+        $residence = $em->getRepository(Residence::class)->find($id);
+        /*        $media = $em->getRepository(Media::class)->find($id);*/
+
+        $count = 0;
+        foreach ($flats as $flat){
+            if ($flat->getStatut() == 'DISPONIBLE'){
+                $count++;
+            }
+        }
+
+        $flats = $residence->getFlats();
+        $prestas = $em->getRepository(Prestation::class)->findAll();
+        $typePrestas = $em->getRepository(TypePresta::class)->findAll();
+        $categoriePrestas = $em->getRepository(CategoriePresta::class)->findAll();
+        return $this->render('MyOrleansBundle::residence.html.twig',[
+            'residence'=>$residence,
+            'flats'=>$flats,
+            'count' => $count,
+            'media' => $media,
+            'prestas'=>$prestas,
+            'typePrestas'=>$typePrestas,
+            'categoriePrestas'=>$categoriePrestas,
+        ]);
+
+
+
+
+    }
+
+}
