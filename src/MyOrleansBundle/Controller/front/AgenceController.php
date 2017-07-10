ac5c69e70a154ae51b21bbd99694b900617f3119
jbh225 <jb.hallassou@gmail.com>
2017-07-10 14:18:07 +0200

    modif generique

--- src/MyOrleansBundle/Controller/front/AgenceController.php
+++ src/MyOrleansBundle/Controller/front/AgenceController.php
@@ -50,15 +50,15 @@ class AgenceController extends Controller
         $em = $this->getDoctrine()->getManager();
       
         $telephone_number = $this->getParameter('telephone_number');
-        $contactForm = $this->createForm('MyOrleansBundle\Form\FormulaireType', $client);
-        $contactForm->handleRequest($request);
+        $formulaire = $this->createForm('MyOrleansBundle\Form\FormulaireType', $client);
+        $formulaire->handleRequest($request);
 
         $partenaires = $em->getRepository(Partenaire::class)->findAll();
         $collaborateurs = $em->getRepository(Collaborateur::class)->findAll();
         $evenements =$em->getRepository(Evenement::class)->findAll();
         $cover = $em->getRepository(Media::class)->findAll();
 
-        if ($contactForm->isSubmitted() && $contactForm->isValid()) {
+        if ($formulaire->isSubmitted() && $formulaire->isValid()) {
 
             $mailer = $this->get('mailer');
 
@@ -94,7 +94,7 @@ class AgenceController extends Controller
                 'collaborateurs'=>$collaborateurs,
                 'evenements'=>$evenements,
                 'cover'=>$cover,
-                'contactForm' => $contactForm->createView()
+                'form' => $formulaire->createView()
 
 
             ]
