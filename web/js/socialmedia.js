f9f7775381e3b7942548a24ec7f09e565ce825ce
jbh225 <jb.hallassou@gmail.com>
2017-07-18 12:39:34 +0200

    suppression btn partage tweeter et ajout btn partage facebook #bugFix113

--- web/js/socialmedia.js
+++ web/js/socialmedia.js
@@ -2,13 +2,14 @@
  * Created by jean-baptiste on 07/07/17.
  */
 //partage facebook
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.9";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.9";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
 $(document).ready(function(){
     $('.collapsible').collapsible();
