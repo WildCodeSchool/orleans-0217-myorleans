93090cb304405deb05e97ade5d541e8eaaff848f
jbh225 <jb.hallassou@gmail.com>
2017-07-19 08:02:07 +0200

    ajout mentions légales et lien vers site web de la wild

--- web/js/script.js
+++ web/js/script.js
@@ -42,3 +42,9 @@ $(document).ready(function () {
         });
     });
 });
+
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
+
